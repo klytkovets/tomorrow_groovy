152888f9a7a55faac61ee8cdbd714b01f2a8859f
Kos <lytkovets.km@gmail.com>
2018-07-19 14:04:55 +0300

    Update BaseTestSpec.groovy

--- src/test/groovy/common/BaseTestSpec.groovy
+++ src/test/groovy/common/BaseTestSpec.groovy
@@ -6,7 +6,7 @@ class BaseTestSpec extends GebReportingSpec{
     def setup(){
         println "starting..."
         if (System.getProperty("env").equals("stage")){
-            browser.driver.get("https://bettersleep:stg-tsleep-@45@staging.tomorrowsleep.com")
+            browser.driver.get("https://staging.tomorrowsleep.com")
         }
     }
 }
